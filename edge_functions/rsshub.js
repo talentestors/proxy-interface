dd31919f3d2ba4eb560dcad7af2d59af9f63b1d9
talentestors <talentestors@gmail.com>
2024-11-14 01:57:46 +0800

    fixed edgefunction rsshub

--- edge_functions/rsshub.js
+++ edge_functions/rsshub.js
@@ -9,11 +9,10 @@ export default async function (request) {
     const resp = new Response(null, {
       status: 204,
     });
-    resp.headers.set("Access-Control-Allow-Origin", "rsshub.app");
+    resp.headers.set("Access-Control-Allow-Origin", "*");
     resp.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     resp.headers.set("Access-Control-Allow-Headers", "Content-Type");
     resp.headers.set("Access-Control-Max-Age", `${86400 * 30}`);
-    resp.headers.set("Host", "rsshub.app");
     return resp;
   }
 
@@ -25,16 +24,21 @@ export default async function (request) {
         newUrl = "";
       }
       console.log(`Process ${request.method} ${newUrl}...`);
-      const res = await fetch("https://rsshub.app" + newUrl, {
+      let res = await fetch("https://rsshub.app" + newUrl, {
         method: "GET",
         headers: {
           "Content-type": "text/html, charset=utf-8, application/json, application/xml, application/rss+xml, application/atom+xml, application/rdf+xml, application/rss, application/atom, application/rdf",
         },
-      });
-      if(url === "" || url === "/" || res.data !== undefined) {
-        res.data = res.data.replace(/"\/(.*?)"(.*?)/g, `https://rsshub.netlify.app`);
+      }).then((res) => res.text());
+      if(res) {
+        // replace example: "/logo.png" to "http://localhost:9999/rsshub/logo.png"
+        res = res.replace(/"\/(.*?)"(.*?)/g,`"https://rsshub.netlify.app/$1"$2`);
       }
-      return res;
+      return new Response(res, {
+        headers: {
+          "content-type": "text/html; charset=utf-8",
+        },
+      });
     } catch (e) {
       console.error(e);
       throw e;
